81baf8460019842060c94edad4d61807cdb27b30
sahilbhandare45 <sahilbhandare90@gmail.com>
2025-12-13 17:20:45 +0530

    First Section Done

---
+++ script.js
@@ -0,0 +1,69 @@
+const follower = document.querySelector('#follower')
+const slidesContainer = document.querySelector(".slides");
+const slides = document.querySelectorAll(".slide");
+let currentIndex = 0;
+const totalSlides = slides.length;
+
+function autoSlide() {
+  currentIndex++;
+
+  if (currentIndex >= totalSlides) {
+    currentIndex = 0;
+  }
+
+  slidesContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+/* Change slide every 4 seconds */
+setInterval(autoSlide, 4000);
+
+const btn = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+  btn.style.display = window.scrollY > 300 ? "block" : "none";
+});
+
+btn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+
+// Mouse Cursor
+(function () {
+  const follower = document.querySelector('#follower');
+
+  let x = 0, y = 0;
+  let mouseX = 0, mouseY = 0;
+  const ease = 0.12;
+
+  function animate() {
+    x += (mouseX - x) * ease;
+    y += (mouseY - y) * ease;
+
+    follower.style.left = x + "px";
+    follower.style.top = y + "px";
+
+    requestAnimationFrame(animate);
+  }
+
+  document.addEventListener("mousemove", (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+  });
+
+  animate();
+})();
+
+
+/* core focus function */
+const cards = document.querySelectorAll('.focus-card');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, { threshold: 0.2 });
+
+cards.forEach(card => observer.observe(card));
